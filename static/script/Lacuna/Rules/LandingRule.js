b88267fa0fa667554ff519f2a55b3264389c7fe6
Diederik Zaaijer <Diederik.Zaaijer@edgile.nl>
2012-04-21 14:45:05 +0200

    First implementation of landing rule.

--- static/script/Lacuna/Rules/LandingRule.js
+++ static/script/Lacuna/Rules/LandingRule.js
@@ -12,7 +12,17 @@ LandingRule.prototype.apply = function (spaceObjects, timeLapse) {
     var ships = this.getFlyingShips(spaceObjects);
     if (!ships || ships.length == 0) return;
 
-    i = 1;
+    for (var i = 0; i < ships.length; i++) {
+        var ship = ships[i];
+        var shipPreviousPosition = ship.getPreviousPosition();
+        if (shipPreviousPosition) {
+            var flightPath = new LineSegment(ship.position.clone(), shipPreviousPosition.clone());
+            var intersectionPoint = LineSegment.intersect(landingPlatform.getLineSegment(), flightPath);
+            if (intersectionPoint) {
+                ship.setStatus(Ship.statusEnum.landing);
+            }
+        }
+    }
 }
 
 LandingRule.prototype.getLandingZone = function (spaceObjects) {
@@ -28,6 +38,13 @@ LandingRule.prototype.getFlyingShips = function (spaceObjects) {
 
     var result = [];
     for (var i = 0, numberOfObjects = spaceObjects.length; i < numberOfObjects; i++) {
-        if (spaceObjects[i] instanceof Ship && spaceObjects[i].getStatus() == Ship.statusEnum.flying) result.push(spaceObjects[i]);
+        var object = spaceObjects[i];
+        if (object instanceof Ship) {
+            if (object.getStatus() == Ship.statusEnum.accelerating ||
+                                    object.getStatus() == Ship.statusEnum.flying) {
+                result.push(object);
+            }
+        }
     }
+    return result;
 }
