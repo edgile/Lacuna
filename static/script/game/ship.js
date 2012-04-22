cf4d06c8ed2955e36c8fe475a985d2cb79a7c6db
ewoudj <ewoudj@gmail.com>
2012-04-23 01:08:37 +0200

    Ship with retention drawing

--- static/script/game/ship.js
+++ static/script/game/ship.js
@@ -13,29 +13,36 @@ var Ship = function(config){
 	this.position = this.position || {x:this.engine.width / 2, y: 50};
     this.maxTailLength = 250;
     this.influencedByGravity = true;
+    this.influencesGravitationalField = false;
     this.setStatus(Ship.statusEnum.accelerating);
     this.setMass(3000);
     this.setDensity(1);
     // Visible elements fields 
     this.modelIndex = 1;
-    this.tail = {type: 'polyline', width: 2, color: 'yellow', points: new PolyLine()};
+    this.tail = {type: 'polyline', position: {x: 0, y: 0}, width: 2, color: 'yellow', points: new PolyLine()};
+    this.ship = {type: 'circle', color: 'yellow', center: {x: 0, y: 0}, radius: this.getRadius(), fill: true};
     this.flightShapes = [
-        {type: 'circle', width: this.getRadius(), color: 'yellow', center: {x: 0, y: 0}, radius: 40, fill: true},
+        this.ship,
         this.tail
     ];
-    this.crashShapes = [];
-	this.shapes = this.flightShapes;
+    this.crashShapes = [
+       {type: 'circle', width: 2, color: 'gray', center: {x: 0, y: 0}, radius: 0},
+       {type: 'circle', width: 2, color: 'gray', center: {x: 0, y: 0}, radius: 0},
+       {type: 'circle', width: 2, color: 'white', center: {x: 0, y: 0}, radius: 0}
+	];
+    this.shapes = this.flightShapes;
 };
 
 Ship.prototype = new SpaceObject();
+Ship.prototype.baseClass = SpaceObject.prototype;
 
 Ship.statusEnum = { finished: 0, accelerating: 1, flying: 2, craching: 3, landing: 4, landed: 5 };
-Ship.statusTimespan = { accelerating: 50, craching: 5, landing: 10 };
+Ship.statusTimespan = { accelerating: 0, craching: 5 };
 
 Ship.prototype.update = function(time){
 	if(this.engine.mode !== 'client'){
 		this.startTime = this.startTime || time;
-		this.elapsedTime = this.startTime - time;
+		this.elapsedTime = time - this.startTime;
 	    var status = this.getStatus();
 	    if (status == Ship.statusEnum.accelerating && this.elapsedTime >= Ship.statusTimespan.accelerating) {
 	        this.setStatus(Ship.statusEnum.flying);
@@ -46,29 +53,31 @@ Ship.prototype.update = function(time){
 	    else if (status == Ship.statusEnum.craching) {
 	        this.getDirection().setLength(((Ship.statusTimespan.craching - this.elapsedTime) / Ship.statusTimespan.craching) * this.getDirection().length());
 	    }
-	    else if (status == Ship.statusEnum.landing && this.elapsedTime >= Ship.statusTimespan.landing) {
-	        this.setStatus(Ship.statusEnum.landed);
-	    }
 	}
+	if (this.getStatus() == Ship.statusEnum.craching) {
+		this.crashShapes[0].radius = Math.max(0, Ship.statusTimespan.craching - this.elapsedTime) * 2;
+		this.crashShapes[1].radius = Math.max(0, Ship.statusTimespan.craching - this.elapsedTime);
+		this.crashShapes[2].radius = Math.max(0, Ship.statusTimespan.craching - this.elapsedTime) * 2 / 3;
+    	this.shapes = this.crashShapes;
+    }
+    else if (this.getStatus() == Ship.statusEnum.landing) {
+        this.ship.color = "red";
+        this.flightShapes.pop();
+    }
+    else if (this.getStatus() == Ship.statusEnum.landed) {
+    	this.ship.color = "green";
+    	this.flightShapes.pop();
+    }
+    else if (this.getStatus() == Ship.statusEnum.finished) {
+        // Do nothing ...
+    }
+    else {
+    	this.shapes = this.flightShapes;
+    }
 };
 
 Ship.prototype.render = function(){
-//	if(this.makeSound){
-//		audio.changeColorAudio.play();
-//		this.makeSound = false;
-//	}
-    if(this.getStatus() == Ship.statusEnum.craching) {
-        this.renderCrache(context);
-    }
-    else {
-        context.fillStyle = this.getGradient(context);
-        context.beginPath();
-        context.arc(this.position.x, this.position.y, this.getRadius(), 0, Math2PI, true);
-        context.fill();
-        context.closePath();
-
-        this.renderTail(context);
-    }
+    
 };
 
 Ship.prototype.getRemoteData = function(){
@@ -82,9 +91,8 @@ Ship.prototype.renderRemoteData = function(remoteData, offset){
 Ship.prototype.setPosition = function (position) {
     // add previous point to the tail.
     if (this.position) {
-        this.tail.points.addPoint(this.position.clone());
+        this.tail.points.addPoint(new THREE.Vector2(this.position.x, this.position.y));
     }
-
     this.baseClass.setPosition.call(this, position);
 
     // limit the length of the tail.
@@ -95,61 +103,22 @@ Ship.prototype.setPosition = function (position) {
     }
 };
 
+Ship.prototype.getPreviousPosition = function () {
+    if (this.tail && this.tail.points.points.length > 1) return this.tail.points.points[this.tail.points.points.length - 2];
+};
+
 Ship.prototype.setStatus = function (status) {
     this.baseClass.setStatus.call(this, status);
 
     this.influencedByGravity = status == Ship.statusEnum.flying || status == Ship.statusEnum.accelerating;
-    this.elapsedTime = 0;
+    this.canCollide = this.influencedByGravity;
+    this.static = this.status == Ship.statusEnum.landed;
+    this.startTime = null;
 };
 
 Ship.prototype.getRadius = function () {
     return 4;
 };
-
-Ship.prototype.getGradient = function (context) {
-    var gradient = context.createRadialGradient(this.position.x, this.position.y, 0, this.position.x, this.position.y, this.getRadius());
-    gradient.addColorStop(0, "white");
-    gradient.addColorStop(1, "yellow");
-
-    return gradient;
-};
-
-Ship.prototype.renderCrache = function (context) {
-    context.strokeStyle = "gray";
-    context.beginPath();
-    context.arc(this.position.x, this.position.y, Math.max(0, Ship.statusTimespan.craching - this.elapsedTime) * 2, 0, Math2PI, true);
-    context.stroke();
-    context.closePath();
-
-    context.strokeStyle = "gray";
-    context.beginPath();
-    context.arc(this.position.x, this.position.y, Math.max(0, (Ship.statusTimespan.craching - this.elapsedTime)), 0, Math2PI, true);
-    context.stroke();
-    context.closePath();
-
-    context.strokeStyle = "white";
-    context.beginPath();
-    context.arc(this.position.x, this.position.y, Math.max(0, (Ship.statusTimespan.craching - this.elapsedTime) * 2 / 3), 0, Math2PI, true);
-    context.stroke();
-    context.closePath();
-};
-
-Ship.prototype.renderTail = function (context) {
-    var localTail = this.tail;
-
-    if (localTail.points.length < 2) return;
-
-    context.strokeStyle = "white";
-    context.lineWidth = 2;
-    context.beginPath();
-    context.moveTo(localTail.points[0].x, localTail.points[0].y);
-    for (var i = 1, numberOfPoints = localTail.points.length; i < numberOfPoints; i++) {
-        context.lineTo(localTail.points[i].x, localTail.points[i].y);
-    }
-    context.stroke();
-    context.closePath();
-};
-
 
 if(typeof(exports) !== 'undefined'){
 	exports.Ship = Ship;
