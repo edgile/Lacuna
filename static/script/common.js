8f3c91bbb773393e4e4c2f0b223d1909133b5b6b
Diederik Zaaijer <Diederik.Zaaijer@edgile.nl>
2012-04-23 22:35:13 +0200

    Use engine/helpers

--- static/script/common.js
+++ static/script/common.js
@@ -2,26 +2,26 @@
 var canvasHeight;
 var Math2PI = Math.PI * 2;
 
-Object.prototype.clone = function () {
-    var newObj = (this instanceof Array) ? [] : {};
-    for (i in this) {
-        if (i == 'clone') continue;
-        if (this[i] && typeof this[i] == "object") {
-            newObj[i] = this[i].clone();
-        } else newObj[i] = this[i]
-    } return newObj;
-};
+//Object.prototype.clone = function () {
+//    var newObj = (this instanceof Array) ? [] : {};
+//    for (i in this) {
+//        if (i == 'clone') continue;
+//        if (this[i] && typeof this[i] == "object") {
+//            newObj[i] = this[i].clone();
+//        } else newObj[i] = this[i]
+//    } return newObj;
+//};
 
-Function.prototype.inheritsFrom = function (parentClassOrObject) {
-    if (parentClassOrObject.constructor == Function) { //Normal Inheritance
-        this.prototype = new parentClassOrObject;
-        this.prototype.constructor = this;
-        this.prototype.baseClass = parentClassOrObject.prototype;
-    }
-    else { //Pure Virtual Inheritance
-        this.prototype = parentClassOrObject;
-        this.prototype.constructor = this;
-        this.prototype.baseClass = parentClassOrObject;
-    }
-    return this;
-};
+//Function.prototype.inheritsFrom = function (parentClassOrObject) {
+//    if (parentClassOrObject.constructor == Function) { //Normal Inheritance
+//        this.prototype = new parentClassOrObject;
+//        this.prototype.constructor = this;
+//        this.prototype.baseClass = parentClassOrObject.prototype;
+//    }
+//    else { //Pure Virtual Inheritance
+//        this.prototype = parentClassOrObject;
+//        this.prototype.constructor = this;
+//        this.prototype.baseClass = parentClassOrObject;
+//    }
+//    return this;
+//};
