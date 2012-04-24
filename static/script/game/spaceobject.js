05a1d5c9ddf9347543d034f4fe939d47491c117b
ewoudj <ewoudj@gmail.com>
2012-04-25 00:59:17 +0200

    Minor corrections

--- static/script/game/spaceobject.js
+++ static/script/game/spaceobject.js
@@ -1,17 +1,9 @@
-/*
- *   Space Object, base for all game entities
- */
-if(typeof(require) !== 'undefined'){
-	var helpers = require("./helpers").helpers;
-	var entity = require("./entity").entity;
-}
-
 SpaceObject = function () {
     this.name = "Unknown";
+
     this.setStatus(SpaceObject.statusEnum.active);
 };
 
-SpaceObject.prototype = new entity();
 SpaceObject.prototype.physicsEntity = true;
 SpaceObject.prototype.position = null;
 SpaceObject.prototype.direction = new THREE.Vector2();
@@ -61,8 +53,9 @@ SpaceObject.prototype.getStatus = function () {
 SpaceObject.prototype.setStatus = function (value) {
     this.status = value;
     if(this.status === 0){
-    	this.finished = true;
+     	this.finished = true;
     }
+
 };
 
 SpaceObject.prototype.isFinished = function () {
@@ -95,116 +88,3 @@ SpaceObject.prototype.getAngle = function (body) {
 
 SpaceObject.prototype.render = function (context2d) {
 };
-
-//function SpaceObject() {
-//    this.name = "Unknown";
-//    this.physicsEntity = true;
-//    this.position = null;
-//    this.direction = new THREE.Vector2();
-//    this.density = 1;
-//    this.mass = 1;
-//    this.setStatus(SpaceObject.statusEnum.active);
-//
-//    this.influencedByGravity = true;
-//    this.canCollide = true;
-//}
-//
-//SpaceObject.statusEnum = {finished: 0, active: 1};
-//
-//SpaceObject.prototype.update = function (timeLapse) {
-//};
-//
-//SpaceObject.prototype = new entity();
-//
-//SpaceObject.prototype.collide = function (body) {
-//    if (!this.canCollide || !body.canCollide) return false;
-//    if (this.isFinished() || body.isFinished()) return false;
-//
-//    return this.getDistance(body) < (this.getRadius() + body.getRadius());
-//};
-//
-//SpaceObject.prototype.setRandomValues = function () {
-//};
-//
-//SpaceObject.prototype.getPosition = function (position) {
-//    return this.position;
-//};
-//
-//SpaceObject.prototype.setPosition = function (position) {
-//    this.position = position;
-//};
-//
-//SpaceObject.prototype.getDirection = function () {
-//    return this.direction;
-//};
-//
-//SpaceObject.prototype.setDirection = function (direction) {
-//    this.direction = direction;
-//};
-//
-//SpaceObject.prototype.setMass = function (mass) {
-//    this.mass = mass;
-//};
-//
-//SpaceObject.prototype.setDensity = function (density) {
-//    this.density = density;
-//};
-//
-//SpaceObject.prototype.getStatus = function () {
-//    return this.status;
-//};
-//
-//SpaceObject.prototype.setStatus = function (value) {
-//    this.status = value;
-//};
-//
-//SpaceObject.prototype.isFinished = function () {
-//    return this.getStatus() === SpaceObject.statusEnum.finished;
-//};
-//
-//SpaceObject.prototype.getRadius = function () {
-//    return Math.pow(0.75 * this.getVolume() * Math.PI, 1 / 3);
-//};
-//
-//SpaceObject.prototype.getVolume = function () {
-//    return this.mass / this.density;
-//};
-//
-//SpaceObject.prototype.getVelocity = function () {
-//    return this.direction.length();
-//};
-//
-//SpaceObject.prototype.getDistance = function (body) {
-//    return this.position.distanceTo(body.position);
-//};
-//
-//SpaceObject.prototype.getGravitationalForce = function (body) {
-//    return gravitationalConstant * this.mass * body.mass * this.getDistanceGravitationFactor(this.getDistance(body));
-//};
-//
-///* Default but less steep than nature! */
-//SpaceObject.prototype.getDistanceGravitationFactor = function (distance) {
-//    return 1 / Math.pow(distance, 1.5);
-//};
-//
-///* Experiment to have gravitational force folow a gaussian curve */
-//SpaceObject.prototype.getDistanceGravitationFactor1 = function (distance) {
-//    var deviation = 25;
-//    var mean = 0;
-//
-//    var factor1 = 1 / deviation * Math.sqrt(Math2PI);
-//    var factor2 = Math.pow(Math.E, (-1 / 2) * Math.pow((distance - mean) / deviation, 2));
-//
-//    return factor1 * factor2;
-//};
-//
-//SpaceObject.prototype.getAngle = function (body) {
-//    var result = new THREE.Vector2();
-//    result.sub(body.position, this.position);
-//    result.normalize();
-//
-//    return result;
-//};
-//
-//SpaceObject.prototype.render = function (context2d) {
-//};
