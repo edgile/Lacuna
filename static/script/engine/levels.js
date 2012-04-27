3537f5af06a604854ed603d230e22ebddc389a63
ewoudj <ewoudj@gmail.com>
2012-04-28 00:07:10 +0200

    Merge

--- static/script/engine/levels.js
+++
@@ -1,9 +0,0 @@
-﻿var Levels = {};
-
-Levels.register = function (identifier, level) {
-    Levels[identifier] = level;
-};
-
-Levels.load = function (config) {
-    return new Levels[config.name](config);
-};
