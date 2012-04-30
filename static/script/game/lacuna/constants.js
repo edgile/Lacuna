35d7c060948d22c5ebcae9f5bd580a08b3417f62
Diederik Zaaijer <Diederik.Zaaijer@edgile.nl>
2012-04-30 21:00:30 +0200

    Different rules for aiMode

--- static/script/game/lacuna/constants.js
+++ static/script/game/lacuna/constants.js
@@ -3,12 +3,21 @@ Constants = {
 	name: "Lacuna",
 	
 	getDefaultRules: function(config){
-		return [new UpdateRule(config), 
-	               new GravitationRule(config), 
-	               new MotionRule(config), 
-	               new CollisionRule(config), 
-	               new LandingRule(config), 
-	               new FlightTimeRule(config),
-	               new DefaultLevelMonitorRule(config)]
+		if(!config.aiMode){
+			return [new UpdateRule(config), 
+		               new GravitationRule(config), 
+		               new MotionRule(config), 
+		               new CollisionRule(config), 
+		               new LandingRule(config), 
+		               new FlightTimeRule(config),
+		               new DefaultLevelMonitorRule(config)]
+		}else{
+			return [new UpdateRule(config), 
+		               new GravitationRule(config), 
+		               new MotionRule(config), 
+		               new CollisionRule(config), 
+		               new LandingRule(config), 
+		               new FlightTimeRule(config)]
+		}
 	}
 };
