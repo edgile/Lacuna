685eff8187856fa02d4fda4e68f2937dca6c2f11
Diederik Zaaijer <Diederik.Zaaijer@edgile.nl>
2012-05-06 17:30:19 +0200

    Added Reset to the menu.

--- static/script/game/lacuna/ui/menus.js
+++ static/script/game/lacuna/ui/menus.js
@@ -34,65 +34,70 @@ var mainMenu = {
 };
 
 var settingsMenu = {
-	'AUDIO': {
-		onMousePlaneUp: function (entity, evt) {
-    		if(Engine.getItem("effectsVolume",'0') === '0'){
-        		Engine.effectsVolume = 10;
-        	}
-        	else{
-        		Engine.effectsVolume = 0;
-        	}
-    		// Persist the setting
-    		Engine.setItem('effectsVolume', Engine.effectsVolume);
-    		window.audio.setVolume(Engine.effectsVolume);
-    		// Update the menu (so it now correctly says 2d/3d)
-    		entity.engine.flow.menu.setItems(settingsMenu);
+    'AUDIO': {
+        onMousePlaneUp: function (entity, evt) {
+            if (Engine.getItem("effectsVolume", '0') === '0') {
+                Engine.effectsVolume = 10;
+            }
+            else {
+                Engine.effectsVolume = 0;
+            }
+            // Persist the setting
+            Engine.setItem('effectsVolume', Engine.effectsVolume);
+            window.audio.setVolume(Engine.effectsVolume);
+            // Update the menu (so it now correctly says 2d/3d)
+            entity.engine.flow.menu.setItems(settingsMenu);
         },
-        getText: function(menu){
-        	var result;
-        	if(Engine.getItem("effectsVolume",'0') === '0'){
-        		result = 'AUDIO: OFF';
-        	}
-        	else{
-        		result = 'AUDIO: ON';
-        	}
-        	return result; 
+        getText: function (menu) {
+            var result;
+            if (Engine.getItem("effectsVolume", '0') === '0') {
+                result = 'AUDIO: OFF';
+            }
+            else {
+                result = 'AUDIO: ON';
+            }
+            return result;
         }
     },
     'VIDEO': {
-    	onMousePlaneUp: function (entity, evt) {
-    		if(Engine.getItem("renderer",'canvas') === 'canvas'){
-    			var webglAvailable = ( function () { 
-    				try { 
-    					return (!! window.WebGLRenderingContext && !! document.createElement( 'canvas' ).getContext( 'experimental-webgl' )); 
-					} catch( e ) { 
-						return false; 
-					} 
-				} )();
-    			if(webglAvailable){
-    				Engine.renderer = 'webgl';
-    			}
-    			else{
-    				alert('Sorry, 3D is not support by your bowser.');
-    			}
-        	}
-        	else{
-        		Engine.renderer = 'canvas';
-        	}
-    		// Persist the setting
-    		Engine.setItem('renderer', Engine.renderer);
-    		// Update the menu (so it now correctly says 2d/3d)
-    		entity.engine.flow.menu.setItems(settingsMenu);
+        onMousePlaneUp: function (entity, evt) {
+            if (Engine.getItem("renderer", 'canvas') === 'canvas') {
+                var webglAvailable = (function () {
+                    try {
+                        return (!!window.WebGLRenderingContext && !!document.createElement('canvas').getContext('experimental-webgl'));
+                    } catch (e) {
+                        return false;
+                    }
+                })();
+                if (webglAvailable) {
+                    Engine.renderer = 'webgl';
+                }
+                else {
+                    alert('Sorry, 3D is not support by your bowser.');
+                }
+            }
+            else {
+                Engine.renderer = 'canvas';
+            }
+            // Persist the setting
+            Engine.setItem('renderer', Engine.renderer);
+            // Update the menu (so it now correctly says 2d/3d)
+            entity.engine.flow.menu.setItems(settingsMenu);
         },
-        getText: function(menu){
-        	var result;
-        	if(Engine.getItem("renderer",'canvas') === 'canvas'){
-        		result = 'VIDEO: 2D';
-        	}
-        	else{
-        		result = 'VIDEO: 3D';
-        	}
-        	return result; 
+        getText: function (menu) {
+            var result;
+            if (Engine.getItem("renderer", 'canvas') === 'canvas') {
+                result = 'VIDEO: 2D';
+            }
+            else {
+                result = 'VIDEO: 3D';
+            }
+            return result;
+        }
+    },
+    'RESET': {
+        onMousePlaneUp: function (entity, evt) {
+            alert('Remove all saved data ...');
         }
     },
     '': {},
